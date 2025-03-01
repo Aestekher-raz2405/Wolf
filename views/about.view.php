767959d9a5cadb163f8c4303d6693919ee29cbf9
Aestekher Mostaque Raz <89532213+Aestekher-raz2405@users.noreply.github.com>
2025-03-01 17:05:34 +0600

    view with raw php

---
+++ views/about.view.php
@@ -0,0 +1,20 @@
+<!--
+  This example requires updating your template:
+
+  ```
+  <html class="h-full bg-gray-100">
+  <body class="h-full">
+  ```
+-->
+<?php require "partials/head.view.php"; ?>
+<?php require "partials/nav.view.php"; ?>
+
+<?php require "partials/banner.view.php"; ?>
+    <main>
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <!-- Your content -->
+            <p>You are in About page .</p>
+        </div>
+    </main>
+<?php require "partials/footer.view.php"; ?>
+
